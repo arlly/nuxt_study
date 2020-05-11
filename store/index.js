1b7d06fafa138532bc4205adc5f6d96dfb104687
FuminoriArimoto <arimoto@queserser.co.jp>
2020-05-11 20:15:44 +0900

    クリックでデータを取得できるように変更した。

--- store/index.js
+++ store/index.js
@@ -1,4 +1,28 @@
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
+export const state = () => ({
+  message: 'count number. ',
+  counter: 0,
+})
+
+export const mutations = {
+  doit(state) {
+    var n = Math.floor(Math.random() * 10);
+    state.counter += n;
+    state.message = 'add ' + n + '.';
+  },
+  reset(stete) {
+    state.counter = 0;
+    stete.message = "reset now."
+  },
+}
+
+export const plugins = [
+  createPersistedState(),
+]
+
+
 
 const createStore = () => {
     return new Vuex.Store({
